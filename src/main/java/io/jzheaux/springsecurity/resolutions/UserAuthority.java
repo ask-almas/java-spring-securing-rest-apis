ec81f2733e1573d4fe1ab16d679f7c55cb2d883f
ask-almas <ask.almas@gmail.com>
2022-11-04 17:27:17 +0100

    Completed module

--- src/main/java/io/jzheaux/springsecurity/resolutions/UserAuthority.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserAuthority.java
@@ -1,4 +1,55 @@
 package io.jzheaux.springsecurity.resolutions;
 
-public class UserAuthority {
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity(name = "authorities")
+public class UserAuthority  {
+    @Id
+    UUID id;
+    @Column
+    String authority;
+    @JoinColumn(name = "username", referencedColumnName = "username")
+    @ManyToOne
+    User user;
+
+    public UserAuthority() {
+    }
+
+    public UserAuthority(UserAuthority userAuthority){
+        this.id = userAuthority.id;
+        this.authority = userAuthority.authority;
+        this.user = userAuthority.user;
+    }
+
+    public UserAuthority(User user, String authority) {
+        this.id = UUID.randomUUID();
+        this.authority = authority;
+        this.user = user;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
