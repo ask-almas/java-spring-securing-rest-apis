b9881a8725473c1478daa55ed54876240d890a05
ask-almas <ask.almas@gmail.com>
2022-11-08 14:19:01 +0100

    Completed module

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
@@ -15,9 +15,15 @@ public class ResolutionInitializer implements SmartInitializingSingleton {
 
 	@Override
 	public void afterSingletonsInstantiated() {
-		this.resolutions.save(new Resolution("Read War and Peace", "user"));
-		this.resolutions.save(new Resolution("Free Solo the Eiffel Tower", "user"));
-		this.resolutions.save(new Resolution("Hang Christmas Lights", "user"));
+		this.resolutions.save(new Resolution("Read War and Peace user", "user"));
+		this.resolutions.save(new Resolution("Free Solo the Eiffel Tower user", "user"));
+		this.resolutions.save(new Resolution("Hang Christmas Lights user", "user"));
+		this.resolutions.save(new Resolution("Read War and Peace hasread", "hasread"));
+		this.resolutions.save(new Resolution("Free Solo the Eiffel Tower hasread", "hasread"));
+		this.resolutions.save(new Resolution("Hang Christmas Lights hasread", "hasread"));
+		this.resolutions.save(new Resolution("Read War and Peace haswrite", "haswrite"));
+		this.resolutions.save(new Resolution("Free Solo the Eiffel Tower haswrite", "haswrite"));
+		this.resolutions.save(new Resolution("Hang Christmas Lights haswrite", "haswrite"));
 
 		User user = new User("user", "{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W");
 		user.grantAuthority("resolution:read");
@@ -31,5 +37,9 @@ public class ResolutionInitializer implements SmartInitializingSingleton {
 		User haswrite = new User("haswrite", "{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W");
 		haswrite.grantAuthority("resolution:write");
 		this.users.save(haswrite);
+
+		User admin = new User("admin","{bcrypt}$2a$10$bTu5ilpT4YILX8dOWM/05efJnoSlX4ElNnjhNopL9aPoRyUgvXAYa");
+		admin.grantAuthority("ROLE_ADMIN");
+		this.users.save(admin);
 	}
 }
